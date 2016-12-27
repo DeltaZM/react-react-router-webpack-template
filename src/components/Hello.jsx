c2f3f8f5a711f869ef06ba5394d2456d04ada71c
John Kwok <volash86@gmail.com>
2016-12-27 20:19:38 +0800

    导入antd ui框架

--- src/components/Hello.jsx
+++ src/components/Hello.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import { Button } from 'antd'
 
 class Hello extends React.Component {
   render () {
     return (
       <div className="hello-div">
         <h1>Hello CVTE</h1>
+        <Button>Hello</Button>
       </div>
     )
   }
